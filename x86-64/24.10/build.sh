73cf958a18c1ec300f582f882609588b61f53e46
zyl19810322 <79440664+zyl19810322@users.noreply.github.com>
2025-04-20 11:15:25 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -24,13 +24,13 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
-PACKAGES="$PACKAGES curl"
+#PACKAGES="$PACKAGES curl"
 #PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 #PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 # 服务——FileBrowser 用户名admin 密码admin
 #PACKAGES="$PACKAGES luci-i18n-filebrowser-go-zh-cn"
-#PACKAGES="$PACKAGES luci-app-argon-config"
-#PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
+PACKAGES="$PACKAGES luci-app-argon-config"
+PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 #24.10
 #PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 #PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
